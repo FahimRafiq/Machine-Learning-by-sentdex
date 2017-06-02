8d371495b3c19f9586e11f0e89ba2fbe0343c49c
Fahim Md Rafiq <fahim6331006@gmail.com>
2017-06-02 14:54:31 +0600

    part 4

--- p04RegressionTrainingAndTesting.py
+++ p04RegressionTrainingAndTesting.py
@@ -1,6 +1,10 @@
 import math
 import pandas as pd
 import quandl
+import numpy as np
+from sklearn import preprocessing, svm
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
 api_key = "Cxe41MDVyJhXQYGuVGx7"
 
@@ -20,9 +24,17 @@ forecast_col = 'Adj. Close'
 df.fillna(value = -99999,inplace = True)
 forecast_out = int(math.ceil(0.01*len(df)))
 
-# print(forecast_out)
-
-
 df['label'] = df[forecast_col].shift(-forecast_out)
 df.dropna(inplace = True)
-# print(df.tail())
+
+X = np.array(df.drop('label',1))
+y = np.array(df['label'])
+
+X = preprocessing.scale(X)
+
+X_train, X_text , y_train , y_test = train_test_split(X,y,test_size=0.2)
+
+clf = LinearRegression()
+clf.fit(X_train,y_train)
+accuracy = clf.score(X_text,y_test)
+print(accuracy)
